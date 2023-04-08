3acf446011cda83dd36590f6a357ed2cfb6427ee
Djordje <djordjemilic33@gmail.com>
2023-04-08 12:15:27 +0200

    added sidebar items with their routes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
-// import { RouterView } from 'vue-router'
+import { RouterView, RouterLink } from 'vue-router'
 import Magnify from 'vue-material-design-icons/Magnify.vue'
 import Bell from 'vue-material-design-icons/Bell.vue'
+import SideMenuItem from './components/SideMenuItem.vue'
 </script>
 
 <template>
@@ -24,5 +25,30 @@ import Bell from 'vue-material-design-icons/Bell.vue'
       <img class="rounded-full w-[33px]" src="./assets/profile.jpg" alt="Image of a person" />
     </div>
   </div>
-  <!-- <RouterView /> -->
+
+  <div id="SideNav" class="fixed w-[240px] bg-[#191922] h-[100svh] border-r border-r-[#32323D]">
+    <div class="w-full pl-6 pt-3 cursor-pointer">
+      <RouterLink to="/">
+        <img width="130" src="/images/deezer-logo.png" alt="" />
+      </RouterLink>
+    </div>
+    <div class="mt-[53px]"></div>
+
+    <SideMenuItem iconString="music" :iconSize="20" pageUrl="/" name="Music" />
+    <SideMenuItem iconString="podcast" :iconSize="20" pageUrl="/podcasts" name="Podcasts" />
+    <SideMenuItem iconString="explore" :iconSize="20" pageUrl="/artist" name="Explore" />
+    <SideMenuItem iconString="favourite" :iconSize="20" pageUrl="/favourite" name="Favourites" />
+
+    <div class="text-[#A2A2AD] font-light text-[12px] pl-[62px] mt-[25px]">
+      <div class="py-[9px] hover:text-[#EF5465] cursor-pointer">Favourite tracks</div>
+      <div class="py-[9px] hover:text-[#EF5465] cursor-pointer">Playlist</div>
+      <div class="py-[9px] hover:text-[#EF5465] cursor-pointer">Albums</div>
+      <div class="py-[9px] hover:text-[#EF5465] cursor-pointer">Artists</div>
+      <div class="py-[9px] hover:text-[#EF5465] cursor-pointer">Podcasts</div>
+    </div>
+  </div>
+
+  <div class="fixed w-[calc(100%-240px)] h-[calc(100%-56px)] ml-[240px] mt-[56px] overflow-x-auto">
+    <RouterView />
+  </div>
 </template>
